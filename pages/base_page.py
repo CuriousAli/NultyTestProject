d0730fb81fe5a240b2b9651aea2a4456c2fa92da
Ali Sadretdinov <sadrali1990@gmail.com>
2022-12-11 01:40:12 +0700

    Changes in locators.py, base_page.py and test_main_page.py; Marked tests are in 'to fix' status

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,20 +1,21 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 
-from .locators import BasePageLocators
+
+from .locators import BasePageLocators, AboutPageLocators
 
 
 
 class BasePage():
-    def __init__(self, browser, url, timeout=5):
+    def __init__(self, browser, url, timeout=15):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
-
 
     def is_element_present(self, how, what):
         try:
@@ -23,13 +24,19 @@ class BasePage():
             return False
         return True
 
-    def is_not_element_present(self, how, what, timeout=4):
+    def is_not_element_present(self, how, what, timeout=15):
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return True
-
         return False
+
+    def scroll_to_the_element(self, how, what):
+        self.element = self.browser.find_element(how, what)
+        self.actions = ActionChains(self.browser)
+        self.actions.move_to_element(self.element).perform()
+
+
 
     def go_to_main_page(self):
         link = self.browser.find_element(*BasePageLocators.MAIN_PAGE_LINK).click()
@@ -43,6 +50,58 @@ class BasePage():
         link = self.browser.find_element(*BasePageLocators.CLIENTS_PAGE_LINK).click()
         assert self.browser.title == "Clients | Nulty | Lighting Design Consultants", " Not a Clients page or title has been changed"
 
-    def go_to_clients_page(self):
-        link = self.browser.find_element(*BasePageLocators.CLIENTS_PAGE_LINK).click()
-        assert self.browser.title == "Clients | Nulty | Lighting Design Consultants", " Not a Clients page or title has been changed"
+    def go_to_about_page(self):
+        link = self.browser.find_element(*BasePageLocators.ABOUT_PAGE_LINK).click()
+        assert self.browser.title == "About | Nulty | Lighting Design Consultants", " Not a About page or title has been changed"
+
+    def go_to_team_page(self):
+        link = self.browser.find_element(*BasePageLocators.TEAM_PAGE_LINK).click()
+        assert self.browser.title == "Team | Nulty | Lighting Design Consultants", " Not a Team page or title has been changed"
+
+    def go_to_blog_page(self):
+        link = self.browser.find_element(*BasePageLocators.BLOG_PAGE_LINK).click()
+        assert self.browser.title == "Blog | Nulty | Lighting Design Consultants", " Not a Blog page or title has been changed"
+
+    def go_to_contact_page(self):
+        link = self.browser.find_element(*BasePageLocators.CONTACT_PAGE_LINK).click()
+        assert self.browser.title == "Contact | Nulty | Lighting Design Consultants", " Not a Contact page or title has been changed"
+
+
+    def should_be_brochure_request_form_from_header(self):
+        self.browser.find_element(*BasePageLocators.BROCHURE_FORM_REQUEST_HEADER).click()
+        assert self.is_element_present(*BasePageLocators.OPENED_BROCHURE_FORM), "Brochure form didn't appear"
+
+    def go_to_cpd_article(self):
+        self.browser.find_element(*BasePageLocators.CPD_LINK).click()
+        assert self.is_element_present(*AboutPageLocators.CPDS_ARTICLE_ACTIVE), "Page has no article about cpd"
+
+    def should_be_newsletter_following_form_from_header(self):
+        self.browser.find_element(*BasePageLocators.NEWSLETTER_FORM_REQUEST).click()
+        assert self.is_element_present(*BasePageLocators.OPENED_NEWSLETTER_FORM), "Newsletter following form didn't appear"
+
+    def success_of_following_newsletters_mailing(self, email):
+        self.browser.find_element(*BasePageLocators.EMAIL_FOR_NEWSLETTER).send_keys(email)
+        self.browser.find_element(*BasePageLocators.NEWSLETTER_SUBMIT_BUTTON).click()
+        assert self.is_element_present(*BasePageLocators.SUCCESS_MESSAGE_OF_FOLLOWING_NEWSLETTERS), "Success message of Following of newsletters not appear"
+
+    def should_be_brochure_form_from_footer(self):
+        self.scroll_to_the_element(*BasePageLocators.FOOTER)
+        self.browser.find_element(*BasePageLocators.BROCHURE_FORM_REQUEST_BUTTON_FOOTER).click()
+        assert self.is_element_present(*BasePageLocators.OPENED_BROCHURE_FORM), "Brochure form didn't appear"
+
+    def success_of_requesting_brochure_footer(self, name, email, message):
+        self.browser.find_element(*BasePageLocators.BROCHURE_FORM_REQUEST_BUTTON_FOOTER).click()
+        self.browser.find_element(*BasePageLocators.NAME_BROCHURE_FORM).send_keys(name)
+        self.browser.find_element(*BasePageLocators.EMAIL_BROCHURE_FORM).send_keys(email)
+        self.browser.find_element(*BasePageLocators.MESSAGE_BROCHURE_FORM).send_keys(message)
+        self.browser.find_element(*BasePageLocators.BROCHURE_SUBMIT_BUTTON).click()
+        assert self.is_element_present(*BasePageLocators.SUCCESS_MESSAGE_OF_REQUESTING_BROCHURE)
+
+    def success_of_requesting_brochure_header(self, name, email, message):
+        self.browser.find_element(*BasePageLocators.BROCHURE_FORM_REQUEST_HEADER).click()
+        self.browser.find_element(*BasePageLocators.NAME_BROCHURE_FORM).send_keys(name)
+        self.browser.find_element(*BasePageLocators.EMAIL_BROCHURE_FORM).send_keys(email)
+        self.browser.find_element(*BasePageLocators.MESSAGE_BROCHURE_FORM).send_keys(message)
+        self.browser.find_element(*BasePageLocators.BROCHURE_SUBMIT_BUTTON).click()
+        assert self.is_element_present(*BasePageLocators.SUCCESS_MESSAGE_OF_REQUESTING_BROCHURE)
+
--- pages/locators.py
+++ pages/locators.py
@@ -3,24 +3,28 @@ from selenium.webdriver.common.by import By
 
 class BasePageLocators():
     BROCHURE_FORM_REQUEST_HEADER = (By.CLASS_NAME, "brochure-stroke")
-    OPENED_BROCHURE_FORM_FROM_HEADER = (By.CLASS_NAME, "brochure-button-content.clearfix.open")
+    OPENED_BROCHURE_FORM = (By.CLASS_NAME, "brochure-button-content.clearfix.open")
     CPD_LINK = (By.CLASS_NAME, "cpd-stroke")
     NEWSLETTER_FORM_REQUEST = (By.CLASS_NAME, "newsletter-stroke")
+    SUCCESS_MESSAGE_OF_FOLLOWING_NEWSLETTERS = (By.CLASS_NAME, "sweet-alert.showSweetAlert.visible")
+    OK_BUTTON_FOR_CLOSE_SUCCESS_MESSAGE = (By.CLASS_NAME, "confirm")
     OPENED_NEWSLETTER_FORM = (By.CSS_SELECTOR, "[id='newsletter-popup'][style='display: block;']")
     EMAIL_FOR_NEWSLETTER = (By.ID, "newsletterpopup_email")
     NEWSLETTER_SUBMIT_BUTTON = (By.ID, "newsletterpopup_button")
     MAIN_PAGE_LINK = (By.CLASS_NAME, "title-area")
-    PROJECTS_PAGE_LINK = (By.ID, "menu-item-29")
-    CLIENTS_PAGE_LINK = (By.ID, "menu-item-28")
-    ABOUT_PAGE_LINK = (By.ID, "menu-item-27")
-    BLOG_PAGE_LINK = (By.ID, "menu-item-32")
-    TEAM_PAGE_LINK = (By.ID, "menu-item-31")
-    CONTACT_PAGE_LINK = (By.ID, "menu-item-30")
-    BROCHURE_FORM_REQUEST_BUTTON_FOOTER = (By.CLASS_NAME, "request-brochure-footer.needsclick")
+    PROJECTS_PAGE_LINK = (By.XPATH, "//*[@id='menu-main-menu-left-1']/li[2]/a")
+    CLIENTS_PAGE_LINK = (By.XPATH, "//*[@id='menu-main-menu-left-1']/li[4]/a")
+    ABOUT_PAGE_LINK = (By.XPATH, "//*[@id='menu-main-menu-left-1']/li[6]/a")
+    TEAM_PAGE_LINK = (By.XPATH, "//*[@id='menu-main-menu-right-1']/li[2]/a")
+    BLOG_PAGE_LINK = (By.XPATH, "//*[@id='menu-main-menu-right-1']/li[4]/a")
+    CONTACT_PAGE_LINK = (By.XPATH, "//*[@id='menu-main-menu-right-1']/li[6]/a")
+    BROCHURE_FORM_REQUEST_BUTTON_FOOTER = (By.XPATH, "/html/body/section[2]/a")
     NAME_BROCHURE_FORM = (By.ID, "input_6_1")
     EMAIL_BROCHURE_FORM = (By.ID, "input_6_2")
     MESSAGE_BROCHURE_FORM = (By.ID, "input_6_9")
-    BROCHURE_SUBMIT_BUTTON = (By.ID,"gform_submit_button_6")
+    BROCHURE_SUBMIT_BUTTON = (By.ID, "gform_submit_button_6")
+    SUCCESS_MESSAGE_OF_REQUESTING_BROCHURE = (By.CLASS_NAME, "gform_confirmation_message_6.gforms_confirmation_message")
+    FOOTER = (By.TAG_NAME, "footer")
 
 
 class MainPageLocators():
@@ -43,7 +47,7 @@ class ProjectsPageLocators():
 
 
 class AboutPageLocators():
-    pass
+    CPDS_ARTICLE_ACTIVE = (By.CLASS_NAME, "tab-title.cpds.active")
 
 
 class BlogPageLocators():
