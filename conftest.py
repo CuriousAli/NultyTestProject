d95b066a6da1d1f84b799f773e4c568ead191dd6
Ali Sadretdinov <sadrali1990@gmail.com>
2022-12-28 17:25:21 +0700

    Change screenshot behavior in conftest.py . RN screenshot are made only for failed test

--- conftest.py
+++ conftest.py
@@ -1,13 +1,34 @@
+import os
 import allure
 import pytest
-import time
-from allure_commons.types import AttachmentType
 from selenium import webdriver
+
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == 'call' and rep.failed:
+        mode = 'a' if os.path.exists('failures') else 'w'
+        try:
+            with open('failures', mode) as f:
+                if 'browser' in item.fixturenames:
+                    web_driver = item.funcargs['browser']
+                else:
+                    print('Fail to take screen-shot')
+                    return
+            allure.attach(
+                web_driver.get_screenshot_as_png(),
+                name='screenshot',
+                attachment_type=allure.attachment_type.PNG
+            )
+        except Exception as e:
+            print('Fail to take screen-shot: {}'.format(e))
 
 @pytest.fixture(scope="function")
 def browser():
     browser = webdriver.Chrome()
+    browser.set_window_size(1700, 1100)
     yield browser
-    with allure.step("Make screenshot"):
-        allure.attach(browser.get_screenshot_as_png(), name=f"{time.strftime('%m-%d-%Y,_%H:%M:%S')}", attachment_type=AttachmentType.PNG)
     browser.quit()
